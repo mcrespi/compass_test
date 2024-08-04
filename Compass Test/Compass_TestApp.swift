e0c3d87db342977cbf5f39fcb3c8f5a99bcd18d3
Martin Crespi <mcrespi@teco.com.ar>
2024-08-04 18:50:38 -0300

    Initial Commit

---
+++ Compass Test/Compass_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Compass_TestApp.swift
+//  Compass Test
+//
+//  Created by Martin Crespi on 04/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Compass_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
